92b4c2d98f3067ae2a7e812ce9438b5f500a6577
castrogotastro <reid.m.gillis@gmail.com>
2023-10-22 14:44:55 -0700

    Fuckyah. Game is working.

--- It_All_Happened_So_Fast/Assets/Scripts/InkManager.cs
+++ It_All_Happened_So_Fast/Assets/Scripts/InkManager.cs
@@ -5,6 +5,7 @@ using Ink.Runtime;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class InkManager : MonoBehaviour
 {
     [SerializeField] 
@@ -36,16 +37,85 @@ public class InkManager : MonoBehaviour
 
     public void DisplayNextLine()
     {
-        if(!_story.canContinue) return;
-
+        if(_story.canContinue)
+        {
         string text = _story.Continue(); //gets next line
         text = text?.Trim(); //removes white space from text
         _textField.text = text; //displays new text
+
+        }
+        else if (_story.currentChoices.Count > 0)
+        {
+            DisplayChoices();
+        }
+
+
     }
+
+
+
+    //Create and display choices. Access choices using _story.currentChoices
+    //NOTE: BUTTON type might need to be changed.
+    private void DisplayChoices()
+    {
+        //checks if choices are already displayed
+        if (_choiceButtonContainer.GetComponentsInChildren<Button>().Length > 0) return;
+
+        for (int i = 0; i < _story.currentChoices.Count; i++) //iterates through all choices
+        {
+            var choice = _story.currentChoices[i];
+            var button = CreateChoiceButton(choice.text); //creates choice button
+
+            button.onClick.AddListener(() => OnClickChoiceButton(choice));
+        }
+    }
+
+
+    Button CreateChoiceButton(string text)
+    {
+        //creates the button from prefab, inserts it into container
+        var choiceButton = Instantiate(_choiceButtonPrefab);
+        choiceButton.transform.SetParent(_choiceButtonContainer.transform, false);
+
+        //sets text on the button
+        var buttonText = choiceButton.GetComponentInChildren<UnityEngine.UI.Text>();
+        buttonText.text = text;
+
+        return choiceButton;
+    }
+
+
+void OnClickChoiceButton(Ink.Runtime.Choice choice)
+{
+    _story.ChooseChoiceIndex(choice.index); //tells ink which choice was selected
+    RefreshChoiceView(); //removes choices from screen
+    DisplayNextLine();
+
+}
+
+void RefreshChoiceView()
+{
+    if (_choiceButtonContainer !=null)
+    {
+        foreach (var button in _choiceButtonContainer.GetComponentsInChildren<UnityEngine.UI.Button>())
+        {
+            Destroy(button.gameObject);
+        }
+    }
+}
+
+
+
+
 
     // Update is called once per frame
     void Update()
     {
         
     }
+
+
+
+
+
 }
