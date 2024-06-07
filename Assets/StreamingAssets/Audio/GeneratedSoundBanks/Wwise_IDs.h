c8cfa1f1188bf5c14779c3d1682284db374cb34a
Aryo Nazaradeh <aryo.nazaradeh@gmail.com>
2024-06-06 23:01:56 -0700

    Fixed issues regarding lost work

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,63 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BARRIERSNAP = 2711589406U;
+        static const AkUniqueID BLIZZARD_LOOP = 2889830528U;
+        static const AkUniqueID CAR1HORN = 4087304941U;
+        static const AkUniqueID CAR2PHONE = 3130223071U;
+        static const AkUniqueID CAR3CRASH = 3818536125U;
+        static const AkUniqueID CARBREAK = 2109667582U;
+        static const AkUniqueID CARDRIVE_LOOP = 1978802756U;
+        static const AkUniqueID CARFALLFADEAWAY = 2920173918U;
+        static const AkUniqueID CARHORN1 = 333258007U;
+        static const AkUniqueID CARHORN2 = 333258004U;
+        static const AkUniqueID CARSLIDE = 2928086120U;
+        static const AkUniqueID ENDINGRUSTLING = 2139003914U;
+        static const AkUniqueID HEARTBEAT = 2179486487U;
+        static const AkUniqueID MEMORY1DOORSLAM = 2163324314U;
+        static const AkUniqueID MEMORY2HOUSEDOOR = 3907936454U;
+        static const AkUniqueID MEMORY3HEARTBEAT = 4012209993U;
+        static const AkUniqueID MEMORY5CRYING = 2196982989U;
+        static const AkUniqueID RINGTONE_START = 684136226U;
+        static const AkUniqueID RINGTONE_STARTDELAYED = 175204128U;
+        static const AkUniqueID RINGTONE_STOP = 2804289306U;
+        static const AkUniqueID SOMESOUND = 3737930646U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID HORNPAN = 269853615U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MASTERBANK = 1542108333U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
